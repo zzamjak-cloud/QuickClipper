117e3d93579ed2caa4b5548c7503da37607fe5ac
jinpyoung <zzamjak@naver.com>
2026-07-06 19:29:56 +0900

    수집기 성공 종료 명시

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -194,7 +194,9 @@ async function main() {
   }
 }
 
-main().catch((err) => {
-  console.error('[collect] 실패:', err);
-  process.exit(1);
-});
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error('[collect] 실패:', err);
+    process.exit(1);
+  });
